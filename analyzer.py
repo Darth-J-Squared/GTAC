23e5559d02381a4ef9402b73edd1e9979fbfce64
Jonathan Hamstra <jhamstra24@my.whitworth.edu>
2024-05-08 16:43:05 -0700

    Etched out code flow, added more graphs, added checkers

--- analyzer.py
+++ analyzer.py
@@ -1,6 +1,8 @@
+import sys
+
 def main():
 
-    file = open("graph_15.txt", "r")
+    file = open("./Euler_Cycle/cycle.txt", "r")
 
     graph = []
     for line in file:
@@ -9,11 +11,32 @@ def main():
     print(graph)
 
     print("Testing for a connected graph:")
-    isconnected = connected(graph)
-    if isconnected:
+    if connected(graph):
         print("The graph is fully connected.")
     else:
-        print("The graph is not fully connected.")
+        print("The graph is not fully connected. Therefore there is no Euler Cycle or Path. Goodbye!")
+        sys.exit()
+    print("Testing if there is a Euler Cycle:")
+    if hascycle(graph):
+        print("Conditions met for Euler Cycle.\nCalculating...")
+        #route = findcycle(graph)
+        #print("Cycle found! Here is the route:")
+        #print(route)
+        #print("This Cycle is also a Euler Path.\nThat is all, goodbye!")
+        #sys.exit()
+    else:
+        print("There is no Euler Cycle present in the graph.\nChecking for Euler Path:")
+        #if(haspath(graph):
+        #   print("Conditions met for Euler Path.\nCalculating...")
+        #   route = findpath(graph)
+        #   print("Path found! Here is the route:")
+        #   print(route)
+        #   print("We have shown that while there is no Euler Cycle, there is still a Euler Path.\nThat is all, goodbye!")
+        #   sys.exit()
+        #else:
+        #   print("Unfortunately, there is no Euler Path either. That is all, goodbye!")
+        #   sys.exit()
+
     file.close()
 
 def connected(graph):
@@ -34,12 +57,24 @@ def connected(graph):
         isconnected = True
     else:
         isconnected = False
-        
     return isconnected
     
+def hascycle(graph):
+    for node in graph:
+        if (sum(node) % 2 != 0):
+            return False
+    return True
+
+def haspath(graph):
+    odd_count = 0
+    for node in graph:
+        if (sum(node) % 2 != 0):
+            odd_count += 1
+    if odd_count != 2:
+        return False
+    return True
 
 
-
-
+ 
 
 main()
