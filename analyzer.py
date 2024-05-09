404a24afc6d0f5da4521bff5ca3b320146f56da3
Jonathan Hamstra <jhamstra24@my.whitworth.edu>
2024-05-09 10:50:07 -0700

    Halfway commented

--- analyzer.py
+++ analyzer.py
@@ -2,9 +2,9 @@ import sys
 import copy
 
 def main():
-
+    #take user selection for which graph
     to_open = input("Select Graph 1 ,2 , 3, or 4.")
-
+    #switch case to pull up the right file
     match to_open:
         case '1':
             file = open("./unconnected/unconnected.txt", "r")
@@ -19,29 +19,29 @@ def main():
 
     graph = []
     for line in file:
-        graph.append([int(x) for x in line.split(",")[0:-1]])
+        graph.append([int(x) for x in line.split(",")[0:-1]])   #pulls each line into a list, throws out the newline character
 
 
     print("Testing for a connected graph:")
-    if connected(graph):
+    if connected(graph): #starts checking in order for the different attributes
         print("The graph is fully connected.")
     else:
         print("The graph is not fully connected. Therefore there is no Euler Cycle or Path. Goodbye!")
         sys.exit()
     print("Testing if there is a Euler Cycle:")
-    if hascycle(graph):
+    if hascycle(graph): #connected returns bool so you can use in an IF statement
         print("Conditions met for Euler Cycle.\nCalculating...")
-        route = findcycle(graph)
+        route = findcycle(graph) #hands it the graph, then takes back a list of instructions
         print("Cycle found! Here is the route:")
         print(route)
         print("This Cycle is also a Euler Path.\nThat is all, goodbye!")
         sys.exit()
     else:
         print("There is no Euler Cycle present in the graph.\nChecking for Euler Path:")
-        path, start = haspath(graph)
-        if (path):
+        path, start = haspath(graph) #since the process to test for a path existing can easily choose a starting node, we take back a bool and a node index
+        if (path): #path is the bool that says if it exists or not
             print("Conditions met for Euler Path.\nCalculating...")
-            route = findcycle(graph, start)
+            route = findcycle(graph, start) #calls the same findcycle function, but provides a specific starting node, namely an odd one
             print("Path found! Here is the route:")
             print(route)
             print("We have shown that while there is no Euler Cycle, there is still a Euler Path.\nThat is all, goodbye!")
@@ -50,23 +50,25 @@ def main():
            print("Unfortunately, there is no Euler Path either. That is all, goodbye!")
            sys.exit()
 
-    file.close()
-
-def connected(graph, start=0, end="null"):
-    size = len(graph[0])
-    reached = [0 for x in range(size)]
+def connected(graph, start=0, end="null"): 
+#takes a graph to be tested, and defaults start to node 1 and end as nothing.
+#this is important because we can dynamically use the function multiple times.
+#if we want to check for connection to every node we only pass the graph
+#if we want to test for bridge, we pass the nodes and a graph without the "bridge"
+    size = len(graph[0]) #checks how many nodes in our graph
+    reached = [0 for x in range(size)] #initializes a list of 0s for every node
     visited = [0 for x in range(size)]
-    reached[start] = 1
-    to_check = [start]
-    while to_check:
-        node = to_check.pop(0)
-        visited[node] = 1
-        for index, connection in enumerate(graph[node]):
-            if index == end and connection:
+    reached[start] = 1 #seeds it by saying we have reached the starting node
+    to_check = [start] #seeds where to go next by saying we need to look at the starting node's children
+    while to_check: #when no more nodes to check, we have no routes left to check
+        node = to_check.pop(0) #takes out the first node in the list
+        visited[node] = 1 #says we have visited it
+        for index, connection in enumerate(graph[node]): #pulls both the index of the node, as well as the connection list
+            if index == end and connection: #checks for if this is our end node in the bridge case, returns true if so
                 return True
-            elif connection and not reached[index]:
-                reached[index] = 1
-                to_check.append(index)
+            elif connection and not reached[index]: #checks if we have a connection to a node we still need
+                reached[index] = 1 #says we have reached this new node
+                to_check.append(index) #
     if all(reached):
         isconnected = True
     else:
